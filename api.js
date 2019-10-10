9b187b26a01594423bcc5bc9b31f0780d3216d36
Muhammad Said Arrafi <muhammadsaidarrafi@gmail.com>
2019-10-10 12:14:54 +0700

    Seting Post ke Database dengan API

--- api.js
+++ api.js
@@ -16,19 +16,40 @@ const databaseName = 'bdg-mongodb'
 
 // mongodbClient.connect(url, option, callback ())
 
-mongodbClient.connect(url, {}, (err, client) =>{
+mongodbClient.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}, (err, client) =>{
     if(err){
         return console.log(err)
     }
 
     const db = client.db(databaseName)
 
+    // POST DATA
+    app.post('/users',(req, res)=>{
+        let {name, role, age} = req.body
+        db.collection('users').insertOne(
+            {    
+                name, 
+                role, 
+                age,
+            }
+        ).then(resp => {
+            res.send({
+                pesan : 'Data berhasil di input',
+                response : {
+                    insertedData : resp.ops[0]
+                            }
+            })
+        }).catch(err =>{
+            res.send(err)
+        })
+    })
+    // Get ALL
 })
 
 
 
 app.listen(port, () => {
-    console.log('API success Running in port '+port)
+    console.log('API success Running in port '+ port)
 })
 
 
