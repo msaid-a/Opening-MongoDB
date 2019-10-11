9a260ea797944dfd88c74e85ff924e742f117845
Muhammad Said Arrafi <muhammadsaidarrafi@gmail.com>
2019-10-11 10:09:45 +0700

    {get onedata, all data}(query), {Edit DAta}

--- api.js
+++ api.js
@@ -23,6 +23,22 @@ mongodbClient.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}, (e
 
     const db = client.db(databaseName)
 
+    // INIT Data
+    app.get('/initdata', (req,res) =>{
+        db.collection('users').insertMany([
+            { name: 'Jhonny', age: 28 },
+            { name: 'Deep', age: 38 },
+            { name: 'Bean', age: 19 },
+            { name: 'Dora', age: 22 },
+            { name: 'Marvel', age: 32 },
+            { name: 'Benjamin', age: 32 },
+        ]).then(resp => {
+            res.send("<h1>Data Init Berhasil di tambahkan</h1>")
+        }).catch(err => {
+            res.send("<h1>Gagal Menambahkan </h1>")
+        })
+    })
+
     // POST DATA
     app.post('/users',(req, res)=>{
         let {name, role, age} = req.body
@@ -44,7 +60,81 @@ mongodbClient.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}, (e
         })
     })
     // Get ALL
+    app.get('/users',(req,res)=>{
+        db.collection('users').find({}).toArray()
+        .then(resp => {
+            
+            if(resp.length === 0){
+                return res.send({messege :'Data Kosong'})
+            }
+
+            res.send(resp)
+        }).catch(err => {
+            res.send(err)
+        })
+    })
+
+    //Home
+    app.get('/',(req,res) => {
+        res.send("<h1>Ini halaman home</h1>")
+    }) 
+
+
+    // get one data with 'QUERY'
+    app.get('/users/one', (req,res) =>{
+    //   req.query = {name, age}
+        let {age,name} =req.query
+
+        age = parseInt(age)
+        db.collection('users').findOne({name, age})
+            .then(resp => {
+                res.send(resp)
+            }).catch (err => {
+                res.send('Data not found')
+            })
+    })
+
+    // GET All Data with 'QUERY'
+    app.get('/users/many',(req,res) => {
+        let { age} = req.query
+        age= parseInt(age)
+        db.collection('users').find({age}).toArray()
+            .then(resp =>{
+                res.send(resp)
+            }).catch(err => {
+                res.send(err)
+            })
+    })
+
+
+    
+    // PUT / EDIT DATA
+    app.put('/users/:name', (req,res) => {
+        // req.params.name (KRITERIA)
+        
+        // DATA BARU
+        // req.body.name
+        // req.body.age
+        db.collection('users').updateOne({
+            name : req.params.name
+        },{ $set : {
+                    name : req.body.name,
+                    age : req.body.age
+                    }
+        } ).then(resp =>{
+            res.send(resp)
+        }).catch (err =>{
+            res.send(err)
+        })
+    })
+
+
+
 })
+
+
+
+
 
 
 
